b68e91da0c41280e9d31277fa22242fa4297b384
soumendu-tito <soumendu.bhattacharya@codeclouds.com>
2025-07-28 16:05:10 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
