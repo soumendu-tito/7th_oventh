681d1b0ed37a492bd47d4f5e152a51b51661f5ac
soumendu-tito <soumendu.bhattacharya@codeclouds.com>
2025-07-28 19:44:35 +0530

    2nd comment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected!');
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+})
+.catch(err => console.error('MongoDB connection error:', err));
+
+// index.js (continued)
+const User = require('./models/User');
+
+app.use(express.json());
+
+app.post('/users', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.get('/users', async (req, res) => {
+  const users = await User.find();
+  res.json(users);
 });
